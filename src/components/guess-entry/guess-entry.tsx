8c78dc99ec28441fab8e3229e32d3a4b75fe1248
jsurato <jared.surato@atomicobject.com>
2022-12-13 16:51:06 -0500

    game state passed down from board

--- src/components/guess-entry/guess-entry.tsx
+++ src/components/guess-entry/guess-entry.tsx
@@ -1,7 +1,8 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { Component, component$, useStylesScoped$ } from "@builder.io/qwik";
 import styles from "./guess-entry.css?inline";
+import { iGuessEntry } from "~/types/game-types";
 
-export const GuessEntry = component$(() => {
+export const GuessEntry:Component<iGuessEntry> = component$(({guess}) => {
 	useStylesScoped$(styles);
-	return <div class="guess-entry">guess entry</div>;
+	return <div class="guess-entry">{guess}</div>;
 });
---
+++ src/types/game-types.ts
@@ -0,0 +1,7 @@
+export interface iGuessColumn {
+  guessEntries: (string | number)[];
+}
+
+export interface iGuessEntry {
+  guess: string | number;
+}
