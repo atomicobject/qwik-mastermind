6500ffeb5f9bafcba8aa9b698033bab74352304d
Jacob Williams <link.williams@icloud.com>
2023-01-24 17:26:17 -0500

    more unit tests

--- src/routes/board/validate.ts
+++ src/routes/board/validate.ts
@@ -4,27 +4,33 @@ import type { RequestHandler } from "@builder.io/qwik-city";
 type GuessValidation = Array<Number>;
 const winningAnswerHardCoded = ["r", "g", "b", "y"];
 
-export const validateGuess = (guessArray: Array<String>, winningAnswer: Array<String>): GuessValidation => {
-    let numCorrect: number = 0;
-    let numMisplaced: number = 0;
+export const validateGuess = (
+	guessArray: Array<String>,
+	winningAnswer: Array<String>
+): GuessValidation => {
+	let numCorrect: number = 0;
+	let numMisplaced: number = 0;
 
-    let filteredAnswer = winningAnswer.filter((letter, index) => letter !== guessArray[index]);
-    let filteredGuess = guessArray.filter((letter, index) => letter !== winningAnswer[index]);
+	let filteredAnswer = winningAnswer.filter(
+		(letter, index) => letter !== guessArray[index]
+	);
+	let filteredGuess = guessArray.filter(
+		(letter, index) => letter !== winningAnswer[index]
+	);
 
-    numCorrect = winningAnswer.length - filteredAnswer.length;
-    
-    for (let i = 0; i < filteredGuess.length; i++) {
-        if (filteredAnswer.includes(filteredGuess[i])) {
-            let firstOccurrence = filteredAnswer.indexOf(filteredGuess[i]);
-            filteredAnswer.splice(firstOccurrence, 1);
-            numMisplaced++;
-        }
-    }
-    return [numCorrect, numMisplaced];
-}
+	numCorrect = winningAnswer.length - filteredAnswer.length;
+
+	for (let i = 0; i < filteredGuess.length; i++) {
+		if (filteredAnswer.includes(filteredGuess[i])) {
+			filteredAnswer.splice(filteredAnswer.indexOf(filteredGuess[i]), 1);
+			numMisplaced++;
+		}
+	}
+	return [numCorrect, numMisplaced];
+};
 
 export const onPost: RequestHandler<Promise<GuessValidation>> = async ({
-  request,
+	request,
 }) => {
-  return validateGuess(await request.json(), winningAnswerHardCoded);
+	return validateGuess(await request.json(), winningAnswerHardCoded);
 };
