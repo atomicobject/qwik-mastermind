32d169367cd1dd5f4b07a9d144536652e214f766
mtirimie1 <maria.irimie@atomicobject.com>
2023-01-24 16:56:05 -0500

    added solution to gameboard

--- src/routes/board/validate.spec.ts
+++ src/routes/board/validate.spec.ts
@@ -1,8 +1,7 @@
 import { validateGuess } from "./validate";
-import { createDOM } from "@builder.io/qwik/testing";
 import { test, expect } from "vitest";
 
-test(`validateGuess should return correct validation value`, async () => {
+test("validateGuess should return correct validation value", async () => {
   const answerOne = ["r", "y", "r", "b"];
   const guessOne = ["r", "r", "r", "r"];
 
@@ -10,23 +9,76 @@ test(`validateGuess should return correct validation value`, async () => {
   expect(resultOne).toStrictEqual([2, 0]);
 });
 
-// test(`[ExampleTest Component]: Should render 💣`, async () => {
-//   const { screen, render } = await createDOM();
-//   await render(<ExampleTest flag={false} />);
-//   expect(screen.outerHTML).toContain("💣");
-// });
+test("validateGuess should return correct validation value", async () => {
+  const answerOne = ["r", "y", "r", "b"];
+  const guessOne = ["r", "b", "r", "y"];
 
-// test(`[ExampleTest Component]: Click counter +1`, async () => {
-//   const { screen, render, userEvent } = await createDOM();
-//   await render(<ExampleTest flag={true} />);
+  const resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([2, 2]);
+});
 
-//   expect(screen.outerHTML).toContain("Count:0");
+test("validateGuess should return correct validation value", async () => {
+  const answerOne = ["r", "y", "r", "b"];
+  const guessOne = ["b", "r", "y", "r"];
 
-//   const spanBefore = screen.querySelector("span") as HTMLDivElement;
-//   await userEvent(".btn-counter", "click");
-//   expect(spanBefore.innerHTML).toEqual("Count:1");
+  const resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([0, 4]);
+});
 
-//   const spanAfter = screen.querySelector("span") as HTMLDivElement;
-//   await userEvent("button", "click");
-//   expect(spanAfter.innerHTML).toEqual("Count:2");
-// });
+test(`validateGuess should return correct validation value`, async () => {
+  // guess is all the same color
+  let answerOne = ["r", "y", "r", "b"];
+  let guessOne = ["r", "r", "r", "r"];
+
+  let resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([2, 0]);
+
+  // answer is all the same color
+  answerOne = ["r", "r", "r", "r"];
+  guessOne = ["r", "y", "r", "b"];
+
+  resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([2, 0]);
+
+  // combination correct, misplaced, and incorrect
+  answerOne = ["r", "y", "r", "b"];
+  guessOne = ["r", "b", "r", "g"];
+
+  resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([2, 1]);
+
+  // all misplaced
+  answerOne = ["r", "y", "r", "b"];
+  guessOne = ["y", "r", "b", "r"];
+
+  resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([0, 4]);
+
+  // guess is all wrong
+  answerOne = ["r", "y", "r", "b"];
+  guessOne = ["g", "g", "g", "g"];
+
+  resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([0, 0]);
+
+  // multiple misplaced for a single correct color
+  answerOne = ["r", "y", "g", "b"];
+  guessOne = ["g", "r", "g", "r"];
+
+  resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([1, 1]);
+
+  // one misplaced for multiple correct color
+  answerOne = ["r", "y", "r", "b"];
+  guessOne = ["g", "r", "g", "y"];
+
+  resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([0, 2]);
+
+  // all correct
+  answerOne = ["r", "r", "r", "r"];
+  guessOne = ["r", "r", "r", "r"];
+
+  resultOne = validateGuess(guessOne, answerOne);
+  expect(resultOne).toStrictEqual([4, 0]);
+});
--- src/routes/board/validate.ts
+++ src/routes/board/validate.ts
@@ -5,21 +5,70 @@ type GuessValidation = Array<Number>;
 
 const winningAnswerHardCoded = ["r", "g", "b", "y"];
 
+const getIndex = (value: String): number => {
+  if (value === "r") {
+    return 0;
+  } else if (value === "y") {
+    return 1;
+  } else if (value === "g") {
+    return 2;
+  } else {
+    return 3;
+  }
+};
+
+const subtractArrays = (
+  arrayOne: Array<number>,
+  arrayTwo: Array<number>,
+  negativesOmmitted?: boolean
+): Array<number> => {
+  return arrayOne.map(function (item, index) {
+    // In this case item correspond to currentValue of array a,
+    // using index to get value from array b
+    const subtract = item - arrayTwo[index];
+    if (subtract < 0 && negativesOmmitted) {
+      return 0;
+    }
+    return subtract;
+  });
+};
+
 export const validateGuess = (
   guessArray: Array<String>,
   winningAnswer: Array<String>
 ): GuessValidation => {
-  let numCorrect: number = 0;
-  let numMisplaced: number = 0;
+  const correctArray = [0, 0, 0, 0]; // completely correct
+  const answerArray = [0, 0, 0, 0]; // winner
+  const incorrectArray = [0, 0, 0, 0]; // not Correct, but possibly almost
+
+  winningAnswer.forEach((g) => {
+    // grab the color count from the true answer
+    answerArray[getIndex(g)]++;
+  });
 
   guessArray.forEach((g, i) => {
-    if (g[i] === winningAnswer[i]) {
-      numCorrect++;
-    } else if (winningAnswer.includes(g[i])) {
-      numMisplaced++;
+    // grab the color count for correct & incorrect
+    if (g === winningAnswer[i]) {
+      correctArray[getIndex(g)]++;
+    } else {
+      incorrectArray[getIndex(g)]++;
     }
   });
-  return [numCorrect, numMisplaced];
+
+  const correctAnswers = correctArray.reduce((a, b) => a + b, 0); // sum of correct array is the number correct
+
+  const remainingArray = subtractArrays(answerArray, correctArray); // the values the end user didn't guess
+  const trulyIncorrectArray = subtractArrays(
+    // the values that the end user got completely wrong
+    remainingArray,
+    incorrectArray,
+    true
+  );
+
+  const incorrectAnswers = trulyIncorrectArray.reduce((a, b) => a + b, 0); // sum of incorrect
+  const almostAnswers = 4 - incorrectAnswers - correctAnswers; // the ones we almost got is all that's left
+
+  return [correctAnswers, almostAnswers]; // returning [correct, almost]
 };
 
 export const onPost: RequestHandler<Promise<GuessValidation>> = async ({
