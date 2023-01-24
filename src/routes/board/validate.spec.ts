8b7bc9d2ecb9d2a98c8c338826b94d9f52cefffe
jsurato <jared.surato@atomicobject.com>
2023-01-24 17:30:48 -0500

    updated tests

--- src/routes/board/validate.spec.ts
+++ src/routes/board/validate.spec.ts
@@ -1,88 +1,74 @@
 import { validateGuess } from "./validate";
 import { test, expect } from "vitest";
 
-test(`validateGuess should return correct validation value`, async () => {
-  // guess is all the same color
-  let answerOne = ["r", "y", "r", "b"];
-  let guessOne = ["r", "r", "r", "r"];
+test(`when guess is correct`, async () => {
+  const answer = ["r", "y", "g", "b"];
+  const guess = ["r", "y", "g", "b"];
 
-  let resultOne = validateGuess(guessOne, answerOne);
-  expect(resultOne).toStrictEqual([2, 0]);
-
-  // answer is all the same color
-  answerOne = ["r", "r", "r", "r"];
-  guessOne = ["r", "y", "r", "b"];
-
-  resultOne = validateGuess(guessOne, answerOne);
-  expect(resultOne).toStrictEqual([2, 0]);
-
-  // combination correct, misplaced, and incorrect
-  answerOne = ["r", "y", "r", "b"];
-  guessOne = ["r", "b", "r", "g"];
-
-  resultOne = validateGuess(guessOne, answerOne);
-  expect(resultOne).toStrictEqual([2, 1]);
-
-  // all misplaced
-  answerOne = ["r", "y", "r", "b"];
-  guessOne = ["y", "r", "b", "r"];
-
-  resultOne = validateGuess(guessOne, answerOne);
-  expect(resultOne).toStrictEqual([0, 4]);
-
-  // guess is all wrong
-  answerOne = ["r", "y", "r", "b"];
-  guessOne = ["g", "g", "g", "g"];
-
-  resultOne = validateGuess(guessOne, answerOne);
-  expect(resultOne).toStrictEqual([0, 0]);
-
-  // multiple misplaced for a single correct color
-  answerOne = ["r", "y", "g", "b"];
-  guessOne = ["g", "r", "g", "r"];
-
-  resultOne = validateGuess(guessOne, answerOne);
-  expect(resultOne).toStrictEqual([1, 1]);
-
-  // one misplaced for multiple correct color
-  answerOne = ["r", "y", "r", "b"];
-  guessOne = ["g", "r", "g", "y"];
-
-  resultOne = validateGuess(guessOne, answerOne);
-  expect(resultOne).toStrictEqual([0, 2]);
-
-  // all correct
-  answerOne = ["r", "r", "r", "r"];
-  guessOne = ["r", "r", "r", "r"];
-
-  resultOne = validateGuess(guessOne, answerOne);
+  const resultOne = validateGuess(guess, answer);
   expect(resultOne).toStrictEqual([4, 0]);
-
-  // test with different letters
-  answerOne = ["r", "y", "g", "b"];
-  guessOne = ["b", "y", "g", "c"];
-
-  resultOne = validateGuess(guessOne, answerOne);
-  expect(resultOne).toStrictEqual([2, 1]);
 });
 
-// test(`[ExampleTest Component]: Should render 💣`, async () => {
-//   const { screen, render } = await createDOM();
-//   await render(<ExampleTest flag={false} />);
-//   expect(screen.outerHTML).toContain("💣");
-// });
+test(`when guess is all incorrect`, async () => {
+  const answer = ["r", "y", "r", "b"];
+  const guess = ["g", "g", "g", "g"];
 
-// test(`[ExampleTest Component]: Click counter +1`, async () => {
-//   const { screen, render, userEvent } = await createDOM();
-//   await render(<ExampleTest flag={true} />);
+  const resultOne = validateGuess(guess, answer);
+  expect(resultOne).toStrictEqual([0, 0]);
+});
 
-//   expect(screen.outerHTML).toContain("Count:0");
+test(`when guess is all the same color, and one is correct`, async () => {
+  const answer = ["r", "y", "g", "b"];
+  const guess = ["r", "r", "r", "r"];
 
-//   const spanBefore = screen.querySelector("span") as HTMLDivElement;
-//   await userEvent(".btn-counter", "click");
-//   expect(spanBefore.innerHTML).toEqual("Count:1");
+  const resultOne = validateGuess(guess, answer);
+  expect(resultOne).toStrictEqual([1, 0]);
+});
 
-//   const spanAfter = screen.querySelector("span") as HTMLDivElement;
-//   await userEvent("button", "click");
-//   expect(spanAfter.innerHTML).toEqual("Count:2");
-// });
+test(`when guess is all the same color, but multiple are correct`, async () => {
+  const answer = ["r", "y", "r", "b"];
+  const guess = ["r", "r", "r", "r"];
+
+  const resultOne = validateGuess(guess, answer);
+  expect(resultOne).toStrictEqual([2, 0]);
+});
+
+test(`when guess is all misplaced`, async () => {
+  const answer = ["r", "y", "r", "b"];
+  const guess = ["y", "r", "b", "r"];
+
+  const resultOne = validateGuess(guess, answer);
+  expect(resultOne).toStrictEqual([0, 4]);
+});
+
+test(`when guess is partially correct and partially misplaced`, async () => {
+  const answer = ["r", "y", "r", "b"];
+  const guess = ["r", "y", "b", "r"];
+
+  const resultOne = validateGuess(guess, answer);
+  expect(resultOne).toStrictEqual([2, 2]);
+});
+
+test(`when guess has multiple misplaced of single correct color`, async () => {
+  const answer = ["r", "y", "g", "b"];
+  const guess = ["g", "r", "g", "r"];
+
+  const resultOne = validateGuess(guess, answer);
+  expect(resultOne).toStrictEqual([1, 1]);
+});
+
+test(`when answer is all same color`, async () => {
+  const answer = ["r", "r", "r", "r"];
+  const guess = ["g", "r", "g", "r"];
+
+  const resultOne = validateGuess(guess, answer);
+  expect(resultOne).toStrictEqual([2, 0]);
+});
+
+test(`testing with more than 4 colors`, async () => {
+  const answer = ["r", "y", "g", "b"];
+  const guess = ["c", "r", "g", "a"];
+
+  const resultOne = validateGuess(guess, answer);
+  expect(resultOne).toStrictEqual([1, 1]);
+});
