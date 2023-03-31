0143722ff8a25aff776980860d33eb0664119547
jsurato <106128382+jsurato@users.noreply.github.com>
2023-03-31 15:37:03 -0400

    generate random answer (#29)
    * generate random answer

--- src/components/game-board/board.tsx
+++ src/components/game-board/board.tsx
@@ -9,12 +9,18 @@ import { GameState } from "../../types/game-types";
 import { ColorSelectDisplay } from "../color-select-display/color-select-display";
 import { GuessColumn } from "../guess-column/guess-column";
 import styles from "./board.css?inline";
+import { ANSWER_SET } from "~/routes/board";
 
 // Create a new context descriptor
 export const MyContext = createContext("my-context");
 
 export const GameBoard = component$(() => {
   useStylesScoped$(styles);
+
+  const answer = Array.from({ length: 4 }, () => {
+    const randomColor = Math.floor(Math.random() * ANSWER_SET.length);
+    return ANSWER_SET[randomColor];
+  });
 
   const gameState: GameState = useStore(
     {
@@ -32,7 +38,7 @@ export const GameBoard = component$(() => {
       ],
       currentColumn: 0,
       lastEnteredRow: -1,
-      answer: ["R", "R", "Q", "R"],
+      answer,
     },
     { recursive: true }
   );
--- src/routes/board/index.tsx
+++ src/routes/board/index.tsx
@@ -1,6 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 import { GameBoard } from "~/components/game-board/board";
 
+export const ANSWER_SET = ["Q", "W", "E", "R", "T", "Y"];
+
 export default component$(() => {
 	return <GameBoard />;
 });
