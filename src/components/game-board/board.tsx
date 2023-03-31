8232221030ec7dfcb671008bf684edf157d09241
jsurato <jared.surato@atomicobject.com>
2023-03-31 15:28:31 -0400

    abstract answer set array make random answer efishent

--- src/components/game-board/board.tsx
+++ src/components/game-board/board.tsx
@@ -9,6 +9,7 @@ import { GameState } from "../../types/game-types";
 import { ColorSelectDisplay } from "../color-select-display/color-select-display";
 import { GuessColumn } from "../guess-column/guess-column";
 import styles from "./board.css?inline";
+import { ANSWER_SET } from "~/routes/board";
 
 // Create a new context descriptor
 export const MyContext = createContext("my-context");
@@ -17,8 +18,8 @@ export const GameBoard = component$(() => {
   useStylesScoped$(styles);
 
   const answer = Array.from({ length: 4 }, () => {
-    const randomColor = Math.floor(Math.random() * 6);
-    return ["Q", "W", "E", "R", "T", "Y"][randomColor];
+    const randomColor = Math.floor(Math.random() * ANSWER_SET.length);
+    return ANSWER_SET[randomColor];
   });
 
   const gameState: GameState = useStore(
@@ -37,7 +38,7 @@ export const GameBoard = component$(() => {
       ],
       currentColumn: 0,
       lastEnteredRow: -1,
-      answer: answer,
+      answer,
     },
     { recursive: true }
   );
